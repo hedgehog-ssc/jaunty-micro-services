6d6c40dad3fdd43d632bf74a24e6ee7aacd63837
hedgehog-ssc <wcs890521@163.com>
2019-11-01 17:45:41 +0800

    1、优化；

--- jaunty-micro-modules/jaunty-micro-system/src/main/java/com/jaunty/controller/SUserController.java
+++ jaunty-micro-modules/jaunty-micro-system/src/main/java/com/jaunty/controller/SUserController.java
@@ -1,9 +1,26 @@
 package com.jaunty.controller;
 
+import com.jaunty.domain.entity.SUser;
+import com.jaunty.service.SUserService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(description = "用户操作接口")
 @RestController
+@RequestMapping("/system/user")
 public class SUserController {
 
+    @Autowired(required = false)
+    private SUserService userService;
+
+    @RequestMapping(value = "getById", method= RequestMethod.GET)
+    @ResponseBody
+    public SUser getById(Long id){
+        return userService.getById(id);
+    }
 
 }
